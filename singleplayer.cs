072fa554dc681db5c5aa376856b6bfd50ecac344
AartiPundalik <aartipundalik68@gmail.com>
2022-12-22 14:47:31 +0530

    first

---
+++ singleplayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeladder
+{
+    internal class singleplayer
+    {
+
+
+       
+            public static void checkposition()
+            {
+                int pos = 0;
+                Console.WriteLine("single player at start position : " + pos);
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+
+
